719a9b2199ce53574f1f3f1ead7eea32518df3f6
sokomishalov <Sokolov.M.Andre@sberbank.ru>
2018-10-22 13:03:17 +0300

    ..

--- lib/api/jokes/jokes.dart
+++ lib/api/jokes/jokes.dart
@@ -10,7 +10,7 @@ Future<List> fetchRandomChuckNorrisJokes() async {
   if (response.statusCode == 200) {
     return json
         .decode(response.body)['value']
-        .map((o) => new Joke(id: o['id'], text: o['joke']))
+        .map((o) => Joke.fromJson(o))
         .toList();
   } else {
     throw Exception('Failed to load jokes :(');
--- lib/model/Joke.dart
+++ lib/model/Joke.dart
@@ -3,4 +3,15 @@ class Joke {
   final String text;
 
   Joke({this.id, this.text});
+
+  factory Joke.fromJson(json) {
+    if (json == null) {
+      return null;
+    } else {
+      return new Joke(
+          id: json['id'],
+          text: json['joke'].toString().replaceAll("&quot;", "\"")
+      );
+    }
+  }
 }
